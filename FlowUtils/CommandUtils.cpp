f9a9099969ce8b310549f478b4a266ba9e1a40d8
Istvan Hetesi <hetesi.istvan@gmail.com>
2018-07-07 09:37:43 +0200

    Changed command validation class to command util.

---
+++ FlowUtils/CommandUtils.cpp
@@ -0,0 +1,9 @@
+#include <CommandUtils.h>
+#include <Arduino.h>
+
+String getCommandId(String& command) {
+	if (isEmpty(command)) {
+
+	}
+	return "";
+}
--- FlowUtils/CommandValidation.cpp
+++
@@ -1,9 +0,0 @@
-#include <CommandValidation.h>
-#include <Arduino.h>
-
-String CommandValidation::getCommandId(String& command) {
-	if (isEmpty(command)) {
-
-	}
-	return "";
-}
