1dbd0b55d752e6497a7666ba2423ec2123d4131e
Istvan Hetesi <hetesi.istvan@gmail.com>
2018-12-13 17:24:57 +0100

    First implementation of a simple list. Will be used by command handlers and drivers.

---
+++ Utils/SimpleList.cpp
@@ -0,0 +1,35 @@
+#include <SimpleList.h>
+
+template<class T>
+SimpleList<T>::SimpleList(Logger& logger, FlowControl& flowControl, byte maxListItems)
+	: logger(logger), flowControl(flowControl) {
+	listItems = new T*[maxListItems];
+}
+
+template<class T>
+bool SimpleList<T>::addItem(T& item) {
+	if (listItemCounter +1 <= maxListItems) {
+		if (!item) {
+			flowControl.handleError(F("Null value can not be added"));
+			return false;
+		}
+		listItems[listItemCounter] = &item;
+		listItemCounter++;
+		logger.logDebug(F("Added new list item"));
+	} else {
+		flowControl.handleError(F("Limit of list items exceeded"));
+		return false;
+	}
+	return true;
+}
+
+template<class T>
+T SimpleList<T>::getItem(byte index) {
+	if (index < maxListItems) {
+		return listItems[index];
+	}
+	else {
+		flowControl.handleError(F("Index out of range"), String(index));
+		return NULL;
+	}
+}
---
+++ Utils/SimpleList.h
@@ -0,0 +1,31 @@
+#ifndef SimpleList_H
+#define SimpleList_H
+
+#include <Logger.h>
+#include <FlowControl.h>
+
+#define MAX_LIST_SIZE 5
+
+template<class T>
+class SimpleList
+{
+public:
+	SimpleList(Logger& logger, FlowControl& flowControl, byte maxListItems = MAX_LIST_SIZE);
+
+	bool addItem(T& item);
+	T getItem(byte index);
+
+private:
+	Logger& logger;
+	FlowControl& flowControl;
+
+	// Shows how many items were already added to the list
+	byte listItemCounter = 0;
+	// Maximum number of the list items
+	byte maxListItems = 0;
+
+	// List items
+	T** listItems;
+};
+
+#endif
