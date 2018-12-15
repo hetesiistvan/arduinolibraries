01545cd0d5096acc293cb19b7b49015e84b725f3
Istvan Hetesi <hetesi.istvan@gmail.com>
2018-12-15 15:41:34 +0100

    Completed build wrapper.

--- buildsketch.sh
+++ buildsketch.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 
+if [ -z $1 ]; then
+	echo "No sketch was specified. Aborting!"
+	exit 1
+fi
+
 if [ -z $ARDUINO_IDE_HOME ]; then
 	echo "ARDUINO_IDE_HOME not defined using default value: /opt/arduino-ide/current"
 	ARDUINO_IDE_HOME=/opt/arduino-ide/current
 fi
 
-${ARDUINO_IDE_HOME}/arduino-builder -hardware ${ARDUINO_IDE_HOME}/hardware -tools ${ARDUINO_IDE_HOME}/hardware/tools/avr -tools ${ARDUINO_IDE_HOME}/tools-builder -libraries /home/kishete/Arduino/libraries -fqbn "arduino:avr:uno" $1
+case "$ARDUINO_TARGET_BOARD" in
+	uno)
+		FQBN=arduino:avr:uno
+		;;
+	nano)
+		FQBN=arduino:avr:nano
+		;;
+	*)
+		echo "ARDUINO_TARGET_BOARD not defined, using default one: arduino:avr:uno"
+		FQBN=arduino:avr:uno
+esac
+
+if [ -z $ARDUINO_LIBRARY_FOLDER ]; then
+	ARDUINO_LIBRARY_FOLDER=$ARDUINO_IDE_HOME/libraries
+fi
+
+echo "Starting build using settings:"
+echo "  ARDUINO_IDE_HOME = $ARDUINO_IDE_HOME"
+echo "  FQBN = $FQBN"
+echo "  ARDUINO_LIBRARY_FOLDER = $ARDUINO_LIBRARY_FOLDER"
+echo "  JAVA_HOME = $JAVA_HOME"
+
+cd $ARDUINO_IDE_HOME
+${ARDUINO_IDE_HOME}/arduino-builder -hardware ${ARDUINO_IDE_HOME}/hardware -tools ${ARDUINO_IDE_HOME}/hardware/tools/avr -tools ${ARDUINO_IDE_HOME}/tools-builder -libraries $ARDUINO_LIBRARY_FOLDER -fqbn "$FQBN" $1
