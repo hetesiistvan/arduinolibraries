f44eeb410894877385bb7b9b37f55c027e90f576
Istvan Hetesi <hetesi.istvan@gmail.com>
2018-12-19 22:56:45 +0100

    Separated build test and push stages

--- build.sh
+++ build.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 
-if [ -z $CI_PROJECT_PATH ]; then
-	# Local build
-	CONTAINER_TAGS="arduinobuild"
-else
-	# Gitlab build
-	CONTAINER_TAGS="$CONTAINER_IMAGE/arduinobuild:$CI_COMMIT_SHA --tag $CONTAINER_IMAGE/arduinobuild:latest"
-fi
+build_image() {
+	if [ -z $CI_PROJECT_PATH ]; then
+		# Local build
+		CONTAINER_TAGS="arduinobuild"
+	else
+		# Gitlab build
+		CONTAINER_TAGS="$CONTAINER_IMAGE/arduinobuild:$CI_COMMIT_SHA --tag $CONTAINER_IMAGE/arduinobuild:latest"
+	fi
+
+	docker build --tag ${CONTAINER_TAGS} .
+}
+
+test_image() {
+	if [ -z $CI_PROJECT_PATH ]; then
+		# Local test
+		CONTAINER_TAG="arduinobuild"
+	else
+		# Gitlab test
+		CONTAINER_TAG="$CONTAINER_IMAGE/arduinobuild:$CI_COMMIT_SHA"
+	fi
+	echo "To be implemented"
+}
 
 case $1 in
-	build_image)
-		docker build --tag ${CONTAINER_TAGS} .
+	build-image)
+		build_image
+	;;
+	test-image)
+		test_image
 	;;
 	*)
 		echo "Invalid parameter given. Aborting!"
