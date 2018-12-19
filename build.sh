adc512690552efce2d8554d477c6e680a686ff2f
Istvan Hetesi <hetesi.istvan@gmail.com>
2018-12-19 09:13:05 +0100

    Taking credentials from Gitlab if they are present

--- build.sh
+++ build.sh
@@ -3,11 +3,22 @@
 BASE_IMAGE=registry.gitlab.com/hetesiistvan/arduino-arduinoci/arduinobuild:latest
 
 pull_base_image() {
-	if [ -z $GITLAB_REGISTRY_TOKEN ]; then
-		GITLAB_REGISTRY_TOKEN=`cat gitlab_registry_token`
+	if [ ! -z $CI_PROJECT_PATH ]; then
+		# Build started from Gitlab, taking the credential from there
+		DOCKER_USER=gitlab-ci-token
+		DOCKER_PASSWD=${CI_JOB_TOKEN}
+	else
+		# Started a local build
+		if [ -z $GITLAB_REGISTRY_TOKEN ]; then
+			GITLAB_REGISTRY_TOKEN=`cat gitlab_registry_token`
+		fi
+		DOCKER_USER=gitlab+deploy-token-33202
+		DOCKER_PASSWD=${GITLAB_REGISTRY_TOKEN}
 	fi
-	docker login -u gitlab+deploy-token-33202 -p ${GITLAB_REGISTRY_TOKEN} registry.gitlab.com
+	docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWD} registry.gitlab.com
+
 	docker pull $BASE_IMAGE
+
 	docker logout
 }
 
